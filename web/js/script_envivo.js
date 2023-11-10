6355553fcdffcbe0e640c03b89781bd5a88940ad
IkerSoto <ik.soto@aulanz.net>
2023-11-10 09:36:05 +0100

    nvivooooo

--- web/js/script_envivo.js
+++ web/js/script_envivo.js
@@ -1,129 +1,153 @@
 document.addEventListener('DOMContentLoaded', function() {
     var contadorPruebas = 0;
+    var tiempoCambioPrueba = 300; // 300 segundos = 5 minutos
 
     cambiarPrueba(); // Inicializa la primera prueba
 
     setInterval(function() {
-        cambiarPrueba(); // Cambiar cada 5 minutos
-    }, 300000);
+        tiempoCambioPrueba--;
+        if (tiempoCambioPrueba === 0) {
+            cambiarPrueba();
+            tiempoCambioPrueba = 300; // Reinicia el contador de tiempo para el próximo cambio de prueba
+        }
+        actualizarContador();
+    }, 1000);
 
     function cambiarPrueba() {
         var pruebaElement = document.querySelector('.prueba');
         var parcialesList = document.querySelector('.parciales ul');
         var tiempoFinalElement = document.getElementById('tiempoFinal');
-        // Define el orden de las pruebas
+        var piscinaElement = document.querySelector('.piscina');
+
         var pruebas = [
             {
-                prueba: 'Prueba 200m Libre',
+                prueba: 'Prueba 200m Libre masculino',
                 parciales: [
-                    'Nadador 1: 1m 00s',
-                    'Nadador 2: 59s',
-                    'Nadador 3: 58s',
-                    'Nadador 4: 56s',
-                    'Nadador 5: 57s',
-                    'Nadador 6: 59s',
-                    'Nadador 7: 1m 02s',
-                    'Nadador 8: 1m 00s'
+                    'Pedro González : 1m 00s',
+                    'Sergio Muñoz : 59s',
+                    'Jorge García : 58s',
+                    'Pablo Sánchez : 56s',
+                    'Juan González : 57s',
+                    'Luis Jiménez : 59s',
+                    'Javier Pérez : 1m 02s',
+                    'Pedro Martínez : 1m 00s'
                 ],
-                tiempoFinal: 'Tiempo Final: 1m 57s'
+                tiempoFinal: 'Tiempo final ganador: Pablo Sánchez 1m 57s',
+                nadadores: [
+                    'González', 'Muñoz', 'García', 'Sánchez',
+                    'González', 'Jiménez', 'Pérez', 'Martínez'
+                ]
             },
             {
-                prueba: 'Prueba 100m Espalda',
+                prueba: 'Prueba 100m Espalda femenino',
                 parciales: [
-                    'Nadador 1: 31s',
-                    'Nadador 2: 30s',
-                    'Nadador 3: 29s',
-                    'Nadador 4: 27s',
-                    'Nadador 5: 27s',
-                    'Nadador 6: 29s',
-                    'Nadador 7: 30s',
-                    'Nadador 8: 29s'
+                    'Lucía Fernández : 31s',
+                    'Sara Gómez  : 30s',
+                    'Isabel Díaz : 29s',
+                    'Carmen Ruiz : 27s',
+                    'Nuria Martín : 27s',
+                    'Rosa López : 29s',
+                    'Ana Fernández : 30s',
+                    'Nerea López : 29s'
                 ],
-                tiempoFinal: 'Tiempo Final: 59s'
+                tiempoFinal: 'Tiempo final ganadora: Nuria Martín 59s',
+                nadadores: [
+                    'Fernández', 'Gómez', 'Díaz', 'Ruiz',
+                    'Martín', 'López', 'Fernández', 'López'
+                ]
             },
             {
-                prueba: 'Prueba 100m Mariposa',
+                prueba: 'Prueba 100m Braza masculino',
                 parciales: [
-                    'Nadador 1: 30s',
-                    'Nadador 2: 32s',
-                    'Nadador 3: 31s',
-                    'Nadador 4: 33s',
-                    'Nadador 5: 29s',
-                    'Nadador 6: 30s',
-                    'Nadador 7: 34s',
-                    'Nadador 8: 32s'
+                    'Juan González : 39s',
+                    'José Antonio : 37s',
+                    'Carlos López : 36s',
+                    'Miguel Hernández : 33s',
+                    'Diego García : 34s',
+                    'David Muñoz : 36s',
+                    'Juan Pérez : 34s',
+                    'Pedro González : 37s'
                 ],
-                tiempoFinal: 'Tiempo Final: 56s'
+                tiempoFinal: 'Tiempo final ganador: Miguel Hernández 1m 08s',
+                nadadores: [
+                    'González', 'Antonio', 'López', 'Hernández',
+                    'García', 'Muñoz', 'Pérez', 'González'
+                ]
             },
             {
-                prueba: 'Prueba 100m Braza',
+                prueba: 'Prueba 100m Mariposa femenino',
                 parciales: [
-                    'Nadador 1: 39s',
-                    'Nadador 2: 37s',
-                    'Nadador 3: 36s',
-                    'Nadador 4: 33s',
-                    'Nadador 5: 34s',
-                    'Nadador 6: 36s',
-                    'Nadador 7: 34s',
-                    'Nadador 8: 37s'
+                    'Marie Daniela : 30s',
+                    'Nuria Martín : 32s',
+                    'Alma Martínez : 28s',
+                    'Laura Sánchez : 33s',
+                    'Lucía Fernández : 29s',
+                    'Eva Rodríguez : 30s',
+                    'Marta Hernández : 34s',
+                    'Elena Rodríguez : 32s'
                 ],
-                tiempoFinal: 'Tiempo Final: 1m 08s'
+                tiempoFinal: 'Tiempo final ganadora: Alma Martínez 58s',
+                nadadores: [
+                    'Daniela', 'Martín', 'Martínez', 'Sánchez',
+                    'Fernández', 'Rodríguez', 'Hernández', 'Rodríguez'
+                ]
             },
             {
-                prueba: 'Prueba 200m Mariposa',
+                prueba: 'Prueba 200m Mariposa masculino',
                 parciales: [
-                    'Nadador 1: 1m 08s',
-                    'Nadador 2: 1m 06s',
-                    'Nadador 3: 1m 04s',
-                    'Nadador 4: 1m 00s',
-                    'Nadador 5: 1m 02s',
-                    'Nadador 6: 1m 07s',
-                    'Nadador 7: 1m 11s',
-                    'Nadador 8: 1m 13s'
+                    'Pablo Sánchez 1: 1m 08s',
+                    'Jorge García 2: 1m 06s',
+                    'Álvaro Martín 3: 1m 04s',
+                    'David Muñoz 4: 1m 00s',
+                    'Fernando González 5: 1m 02s',
+                    'Diego García 6: 1m 07s',
+                    'Luis Jiménez 7: 1m 11s',
+                    'Xabier González 8: 1m 13s'
                 ],
-                tiempoFinal: 'Tiempo Final: 2m 09s'
-            },
-            {
-                prueba: 'Prueba 200m Espalda',
-                parciales: [
-                    'Nadador 1: 1m 08s',
-                    'Nadador 2: 1m 05s',
-                    'Nadador 3: 1m 04s',
-                    'Nadador 4: 1m 24s',
-                    'Nadador 5: 1m 18s',
-                    'Nadador 6: 1m 41s',
-                    'Nadador 7: 1m 37s',
-                    'Nadador 8: 1m 15s'
-                ],
-                tiempoFinal: 'Tiempo Final: 2m 45s'
-            },
+                tiempoFinal: 'Tiempo final ganador: Fernando González 2m 09s',
+                nadadores: [
+                    'Sánchez', 'García', 'Martín', 'Muñoz',
+                    'González', 'García', 'Jiménez', 'González'
+                ]
+            }
         ];
 
-        // Escoge la prueba en el orden establecido
-        var nuevaPrueba = pruebas[contadorPruebas];
 
-        // Incrementa el contador para la siguiente prueba
-        contadorPruebas = (contadorPruebas + 1) % pruebas.length;
+    var nuevaPrueba = pruebas[contadorPruebas];
 
-        // Cambia el contenido en la página
-        pruebaElement.textContent = nuevaPrueba.prueba;
+    contadorPruebas = (contadorPruebas + 1) % pruebas.length;
 
-        // Limpia los parciales anteriores
-        parcialesList.innerHTML = '';
+    pruebaElement.textContent = nuevaPrueba.prueba;
 
-        // Agrega los nuevos parciales
-        nuevaPrueba.parciales.forEach(function(parcial) {
-            var nuevoItem = document.createElement('li');
-            nuevoItem.textContent = parcial;
-            parcialesList.appendChild(nuevoItem);
-        });
+    parcialesList.innerHTML = '';
 
-        // Cambia el tiempo final
-        tiempoFinalElement.textContent = nuevaPrueba.tiempoFinal;
-  // Espera dos minutos antes de mostrar el tiempo final
-  tiempoFinalElement.style.display = 'none'; // Ocultar tiempo final
-  setTimeout(function() {
-      tiempoFinalElement.style.display = 'block'; // Mostrar tiempo final
-  }, 120000); // 120000 milisegundos = 2 minutos
+    nuevaPrueba.parciales.forEach(function(parcial) {
+        var nuevoItem = document.createElement('li');
+        nuevoItem.textContent = parcial;
+        parcialesList.appendChild(nuevoItem);
+    });
+
+    piscinaElement.innerHTML = ''; 
+
+    nuevaPrueba.nadadores.forEach(function(tiempo, indice) {
+        var nuevoNadador = document.createElement('div');
+        nuevoNadador.className = 'calle';
+        nuevoNadador.textContent = nuevaPrueba.nadadores[indice];
+        piscinaElement.appendChild(nuevoNadador);
+    });
+
+    tiempoFinalElement.textContent = nuevaPrueba.tiempoFinal;
+
+    tiempoFinalElement.style.display = 'none';
+    setTimeout(function() {
+        tiempoFinalElement.style.display = 'block';
+    }, 120000);
+}
+
+function actualizarContador() {
+    var contadorElement = document.getElementById('contadorCambioPrueba');
+    var minutos = Math.floor(tiempoCambioPrueba / 60);
+    var segundos = tiempoCambioPrueba % 60;
+    contadorElement.textContent = 'Próximo cambio de prueba en: ' + minutos + ' minutos ' + segundos + ' segundos';
 }
 });
